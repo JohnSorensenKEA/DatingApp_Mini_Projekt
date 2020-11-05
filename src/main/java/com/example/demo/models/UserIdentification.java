a8728ad8b9216d70af78aea9c50e118e358c243f
John <john383p@stud.kea.dk>
2020-11-05 12:34:04 +0100

    changed UserIdentification

--- src/main/java/com/example/demo/models/UserIdentification.java
+++ src/main/java/com/example/demo/models/UserIdentification.java
@@ -1,4 +1,37 @@
 package com.example.demo.models;
 
 public class UserIdentification {
+    private String cookieID;
+    private int userID;
+    private boolean admin;
+
+    public UserIdentification(String cookieID, int userID, boolean admin) {
+        this.cookieID = cookieID;
+        this.userID = userID;
+        this.admin = admin;
+    }
+
+    public String getCookieID() {
+        return cookieID;
+    }
+
+    public void setCookieID(String cookieID) {
+        this.cookieID = cookieID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
 }
